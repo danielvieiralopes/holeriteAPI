e6a25bde1927c92ed602b38fbef6310170dbbc37
danielvieiralopes <danielvieira.2007@hotmail.com>
2025-04-12 01:52:27 -0300

    ajustandoAppsettings

--- obj/Debug/net9.0/HoleriteApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/HoleriteApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HoleriteApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f3eedd7a445d75e629a6b05ec73c8844eea5920")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a628a47d4d6f6a9f04f8ac8d79b4dce06af64b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HoleriteApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HoleriteApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
